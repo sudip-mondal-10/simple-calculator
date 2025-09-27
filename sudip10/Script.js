d99539b9dfcbc9fbfe15775b8acc16516f704ceb
sudip-mondal-10 <sudipm7779@gmail.com>
2025-09-27 16:21:54 +0530

    Add files via upload

---
+++ sudip10/Script.js
@@ -0,0 +1,13 @@
+Let result=document.querySelector('.result');
+
+function appendToResult(value){
+	result.value+=value;
+}
+
+function ClearResult(){
+	result.value='';
+}
+ 
+functionv CalculateResulT(){
+	result.value=evel(result.value);
+}
